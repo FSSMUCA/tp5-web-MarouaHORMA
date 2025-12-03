bf29fafc1080cbe1726fb3bc0da6a00b0ba1c111
MarouaHORMA <m.horma939m.horma3939@uca.ac.ma>
2025-12-03 19:37:14 +0100

    Ajout exercice2 JS

---
+++ exo2.js
@@ -0,0 +1,10 @@
+
+let premon = "sara" ;
+let age = 22 ;
+
+let phrase = "je m'appelle " + premon + " et j'ai " + age + " ans.";
+console.log(phrase);
+
+let newage = 23 ;
+phrase = `je m'appelle ${premon} et j'ai ${newage} ans.`;
+console.log(phrase);
